48de32496bd4eecc2fb432ae9dfffbb3cd737061
Mauricio-Maxuel <mauricio.msantos@sptech.school>
2022-11-06 18:49:00 -0300

    retornando todos os clientes com o Jakarta Persistence

--- src/main/java/com/maumas/maumaslog/maxuellogapi/api/controller/ClienteController.java
+++ src/main/java/com/maumas/maumaslog/maxuellogapi/api/controller/ClienteController.java
@@ -1,29 +1,24 @@
 package com.maumas.maumaslog.maxuellogapi.api.controller;
 
 import com.maumas.maumaslog.maxuellogapi.domain.model.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
+import javax.persistence.EntityManager;
 import java.util.List;
 
 @RestController
 public class ClienteController {
 
+    @Autowired
+    private EntityManager entityManager;
+
+
     @GetMapping("/clientes")
     public List<Cliente> listar() {
-        Cliente cliente1 = new Cliente();
-        cliente1.setId(1L);
-        cliente1.setNome("Joã0");
-        cliente1.setEmail("teste@.gmail.com");
-        cliente1.setTelefone("11 945999-11111");
-
-        Cliente cliente2 = new Cliente();
-        cliente2.setId(2L);
-        cliente2.setNome("Maria");
-        cliente2.setTelefone("11 945999-11111");
-        cliente2.setEmail("maria@.gmail.com");
-
-        return Arrays.asList(cliente1, cliente2);
+        return entityManager.createQuery("from Cliente", Cliente.class).getResultList();
     }
+
+    
 }
--- src/main/java/com/maumas/maumaslog/maxuellogapi/domain/model/Cliente.java
+++ src/main/java/com/maumas/maumaslog/maxuellogapi/domain/model/Cliente.java
@@ -1,15 +1,28 @@
 package com.maumas.maumaslog.maxuellogapi.domain.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+import java.util.Objects;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
+@Table(name = "cliente")
 public class Cliente {
-    private Long id;
-    private String nome;
-    private String email;
-    private String telefone;
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private String telefone;
 
 }
