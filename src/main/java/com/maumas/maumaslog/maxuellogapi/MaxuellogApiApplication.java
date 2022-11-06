d1b83d40b79c5db6b9d04436d9b26dcdb7a09ac9
Mauricio-Maxuel <mauricio.msantos@sptech.school>
2022-11-06 17:21:20 -0300

    first commit

---
+++ src/main/java/com/maumas/maumaslog/maxuellogapi/MaxuellogApiApplication.java
@@ -0,0 +1,13 @@
+package com.maumas.maumaslog.maxuellogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxuellogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaxuellogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maumas/maumaslog/maxuellogapi/MaxuellogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maumas.maumaslog.maxuellogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxuellogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
