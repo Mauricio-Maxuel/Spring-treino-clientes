c06e8a70974ab111be0e56d1d21361d947fca48a
Mauricio-Maxuel <mauricio.msantos@sptech.school>
2022-11-06 19:47:13 -0300

    Adicionando o Bean Validation

--- src/main/java/com/maumas/maumaslog/maxuellogapi/domain/model/Cliente.java
+++ src/main/java/com/maumas/maumaslog/maxuellogapi/domain/model/Cliente.java
@@ -5,6 +5,10 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -19,10 +23,18 @@ public class Cliente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
+    @Size(max = 60)
     private String nome;
 
+    @NotBlank
+    @Email
+    @Size(max = 255)
     private String email;
 
+
+    @NotBlank
+    @Size(max = 20)
     private String telefone;
 
 }
